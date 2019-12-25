996d6445e9cdba7d3d1c54681457ac30e7f046dc
shaocui <NLe$rm#1*TT@>
2019-12-25 17:17:58 +0800

    整合apollo框架初始化

--- function-one/src/main/java/com/demo/OneApplication.java
+++ function-one/src/main/java/com/demo/OneApplication.java
@@ -1,13 +1,13 @@
 package com.demo;
 
-//import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan({"mapper"})  //项目启动时会扫描mapper包及子包的接口，否则会报错找不到mapper文件
-//@EnableApolloConfig
+@EnableApolloConfig
 public class OneApplication {
 
     public static void main(String[] args) {
